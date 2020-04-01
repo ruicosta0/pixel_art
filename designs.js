9cdd0811d16b9d7bd0d128544a712c9759a2fc0f
ruicosta0 <62068297+ruicosta0@users.noreply.github.com>
2020-04-01 11:54:49 +0100

    Add files via upload

--- designs.js
+++ designs.js
@@ -4,16 +4,18 @@
 const tblBody = document.createElement("tbody");
 const tableData = document.getElementsByTagName("td");
 const checkColor = "";
-//const grid = document.getElementsByTagName("tr");
 
-//Cell is filled with color specified in the colorPicker when user clicks within grid. Function triggered by listener held in makeGrid function. 
+/*
+@ desc Cells are filled with color specified in the colorPicker
+@ param evt to enable reference to the target of the event or cell to color
+*/
 function colorFillByClick (evt) {
     const color = document.getElementById("colorPicker").value;
     evt.target.bgColor=color;
 }
 
 //Function clears out any colored cells within the grid when submit button on form is pressed
-function resetGrid () { 
+function resetGrid () {
     for (let c = 0; c < tableData.length; c++) {
         if (tableData[c].bgColor != checkColor) {
            tableData[c].bgColor = "";
@@ -22,30 +24,36 @@ function resetGrid () {
     }
 }
 
-//Function creates a grid using the height and width inputs on the form when submit button is pressed
+/*
+ * @desc Creates a grid of squares when submit button is pressed
+ * @param int $height - number of squares representing the height of the grid
+ * @param int $width - number of squares representing the width of the grid 
+*/
 function makeGrid(height, width) {
-    for (let r = 0; r < height;  r++) {                                       
+    tblBody.innerHTML = "";
+    for (let r = 0; r < height;  r++) {                                 
           var newRow = document.createElement("tr");
-          for (let w = 0; w < width; w++) {     
-              var newCell = document.createElement("td");    
-              newRow.appendChild(newCell);                                          
+          for (let w = 0; w < width; w++) {
+              var newCell = document.createElement("td");
+              newRow.appendChild(newCell);                              
           }
-      tblBody.appendChild(newRow);                                                   
-      }
-    pixelCanvas.appendChild(tblBody);                                                
-    event.preventDefault();
-    tblBody.addEventListener('click', colorFillByClick)                              
+      tblBody.appendChild(newRow);                                              
     }
-    
-/* If the table size is equal to the (height * width) on the form then call resetGrid to clear out the colored cells, 
-otherwise create a grid by calling makeGrid function */
+    pixelCanvas.appendChild(tblBody);                                    
+    event.preventDefault();
+    tblBody.addEventListener('click', colorFillByClick)                     
+}
+
+/*
+* @desc If the table size is equal to the (height * width) on the form then call resetGrid to clear colors
+* otherwise create a grid by calling makeGrid function
+*/
 sizePicker.addEventListener('submit', function(event) {
     if (tableData.length == (inputHeight.value*inputWidth.value)) {
         resetGrid();
     } else {
-      document.getElementById("pixelCanvas").innerHTML = "";
       let height = document.getElementById("inputHeight").value;
       let width = document.getElementById("inputWidth").value;
-      makeGrid(height, width); 
-      }
+      makeGrid(height, width);
+    }
 })
