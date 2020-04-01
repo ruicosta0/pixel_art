dacea62c0b1c932b03d89d893c862a31541413f9
ruicosta0 <62068297+ruicosta0@users.noreply.github.com>
2020-04-01 11:59:30 +0100

    Add files via upload

--- designs.js
+++ designs.js
@@ -1,46 +1,59 @@
 // Select color input
 // Select size input
 
-// When size is submitted by the user, call makeGrid()
+const tblBody = document.createElement("tbody");
+const tableData = document.getElementsByTagName("td");
+const checkColor = "";
 
-
-// When a square is clicked on it is filled in with the color the user has chosen on the colorPicker
+/*
+@ desc Cells are filled with color specified in the colorPicker
+@ param evt to enable reference to the target of the event or cell to color
+*/
 function colorFillByClick (evt) {
     const color = document.getElementById("colorPicker").value;
     evt.target.bgColor=color;
 }
 
-
-// Function checks which squares contain color and then removes the color. If there is no color, nothing happens.
-function clearColor (tableData) {
-    const checkColor = "";
+//Function clears out any colored cells within the grid when submit button on form is pressed
+function resetGrid () {
     for (let c = 0; c < tableData.length; c++) {
         if (tableData[c].bgColor != checkColor) {
            tableData[c].bgColor = "";
         }
-    event.preventDefault();
+     event.preventDefault();
     }
 }
 
-// Function checks if tableData exists already and if yes, executes clearColor function, else creates grid
-function makeGrid(event) {
-    const tableData = document.getElementsByTagName("td");
-    if (tableData.length > 0) {
-        clearColor(tableData);
-    } else {
-      const tblBody = document.createElement("tbody");                               // Table body is created
-      for (let r = 0; r < document.getElementById("inputHeight").value; r++) {       // Loop uses height input to generate table rows
+/*
+ * @desc Creates a grid of squares when submit button is pressed
+ * @param int $height - number of squares representing the height of the grid
+ * @param int $width - number of squares representing the width of the grid 
+*/
+function makeGrid(height, width) {
+    tblBody.innerHTML = "";
+    for (let r = 0; r < height;  r++) {                                 
           var newRow = document.createElement("tr");
-          for (let w = 0; w < document.getElementById("inputWidth").value; w++) {    // Loop uses width input to generate table data
-               var newCell = document.createElement("td");
-               newRow.appendChild(newCell);                                          // Table data/cells are appended to the rows
+          for (let w = 0; w < width; w++) {
+              var newCell = document.createElement("td");
+              newRow.appendChild(newCell);                              
           }
-      tblBody.appendChild(newRow);                                                   // Rows appended to the table body
-      }
-    pixelCanvas.appendChild(tblBody);                                                // Table body appended to the table
-    event.preventDefault();
-    tblBody.addEventListener('click', colorFillByClick)                              // Listener calls colorFillByClick function if user clicks in grid
+      tblBody.appendChild(newRow);                                              
     }
+    pixelCanvas.appendChild(tblBody);                                    
+    event.preventDefault();
+    tblBody.addEventListener('click', colorFillByClick)                     
 }
 
-sizePicker.addEventListener('submit', makeGrid)           // on user clicking submit makeGrid function is called
+/*
+* @desc If the table size is equal to the (height * width) on the form then call resetGrid to clear colors
+* otherwise create a grid by calling makeGrid function
+*/
+sizePicker.addEventListener('submit', function(event) {
+    if (tableData.length == (inputHeight.value*inputWidth.value)) {
+        resetGrid();
+    } else {
+      let height = document.getElementById("inputHeight").value;
+      let width = document.getElementById("inputWidth").value;
+      makeGrid(height, width);
+    }
+})
